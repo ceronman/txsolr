c051db3f33caafec10b4477f23e69dadd64c26cd
Manuel Cerón <manuel@fluidinfo.com>
2010-09-07 15:01:51 -0500

    Updated tests for client

--- test/test_client.py
+++ test/test_client.py
@@ -1,6 +1,8 @@
 from twisted.trial import unittest
+from twisted.internet import defer
 
 from txsolr.client import SolrClient
+from txsolr.errors import WrongResponseCode
 
 # FIXME: avoid hardcoded url
 SOLR_URL = 'http://localhost:8983/solr/'
@@ -10,5 +12,24 @@ class ClientTest(unittest.TestCase):
     def setUp(self):
         self.client = SolrClient(SOLR_URL)
 
-    def test_request(self):
-        return self.client._request('GET', '', {}, None)
+    def test_wrongResponseRequest(self):
+        result = defer.Deferred()
+
+        d = self.client._request('GET', '', {}, None)
+
+        def callback(content):
+            result.errback('Request should fail')
+        d.addCallback(callback)
+
+        def errback(failure):
+            if isinstance(failure.value, WrongResponseCode):
+                result.callback(None)
+            else:
+                result.errback('Error should be WrongResponseCode')
+        d.addErrback(errback)
+
+        return result
+
+    def test_add(self):
+        document = {'id': 1, 'text': 'manuel ceron'}
+        return self.client.add([document])
