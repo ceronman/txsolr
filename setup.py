a2d41c97d4068d66e25427f0c8a22adfd3a7105a
Manuel Cerón <manuel@fluidinfo.com>
2010-12-13 12:00:08 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,21 @@ setup(
     version = '0.1.0',
     description = 'Twisted-based asynchronous client library for Solr'\
                   'Enterprise Search Server',
-    packages = ['txsolr'],
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Text Processing :: Indexing',
+    ],
+
     author = 'Manuel Cerón',
     author_email = 'manuel@fluidinfo.com',
     url = 'https://launchpad.net/txsolr',
+
+    packages = ['txsolr'],
 )
